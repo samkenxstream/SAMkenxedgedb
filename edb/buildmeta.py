rohankumardubey/edgedb

Initial draft of wasm server integration

edb/buildmeta.py
@@ -44,7 +44,7 @@ from edb.common import verutils
 
 
 # Increment this whenever the database layout or stdlib changes.
-EDGEDB_CATALOG_VERSION = 2022_03_01_00_00
+EDGEDB_CATALOG_VERSION = 2022_03_14_00_00
 EDGEDB_MAJOR_VERSION = 2
 
 

samkenxstream/SAMkenxedgedb

edb/buildmeta.py
@@ -44,7 +44,7 @@ from edb.common import verutils
 
 
 # Increment this whenever the database layout or stdlib changes.
-EDGEDB_CATALOG_VERSION = 2022_04_15_00_01
+EDGEDB_CATALOG_VERSION = 2022_04_19_00_00
 EDGEDB_MAJOR_VERSION = 2
 
 